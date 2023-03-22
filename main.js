5de5665b6cb215be0154120989a664f69db25783
Uladzislau Sobal <sobolvladsav@gmail.com>
2023-03-22 12:28:04 +0300

    added package.json and initial files

---
+++ main.js
@@ -0,0 +1,7 @@
+const OsuMatchParser = require('./src/osuMatchParser');
+
+function main(){
+    console.log("Program started");
+
+}
+main();
---
+++ src/osuMatchParser.js
@@ -0,0 +1,5 @@
+class OsuMatchParser{
+    
+
+}
+module.exports = OsuMatchParser;
