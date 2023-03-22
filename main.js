59ac82b3572dd2f6bb1db59598a9a99a4ee7acd2
Uladzislau Sobal <sobolvladsav@gmail.com>
2023-03-22 15:48:22 +0300

    added page parsing

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const OsuMatchParser = require('./src/osuMatchParser');
 
 function main(){
+    const matchLinks = [];
     console.log("Program started");
+    let osuMParser = new OsuMatchParser([], matchLinks);
+    osuMParser.parse().then((res)=>{
+        console.log('endeded')
+    });
 
 }
 main();
--- src/osuMatchParser.js
+++ src/osuMatchParser.js
@@ -1,5 +1,45 @@
-class OsuMatchParser{
-    
+const HTMLParser  = require('node-html-parser');
+
+class OsuMatchParser {
+
+    mode;
+    matchLinks;
+
+    constructor(mode, mLinks) {
+        this.mode = mode;
+        this.matchLinks = mLinks;
+    }
+    async parse() {
+        let users = [];
+        for (let link of this.matchLinks) {
+            try {
+                const response = await fetch(link, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'text/html;charset=utf-8'
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error("HTTP status " + response.status);
+                }
+                const result = await response.text();           
+                //console.log(result);    
+                var root = HTMLParser.parse(result);
+                console.log(root.querySelectorAll(".js-react--mp-history"));                
+
+
+                //getting users of the match
+                /*let json = JSON.parse(root.querySelector('#json-events').rawText);
+                users = json.users;*/
+                
+            } catch (e) {
+                console.error(e);
+            }
+
+        }
+
+    }
+
 
 }
 module.exports = OsuMatchParser;
