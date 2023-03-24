d259661dcb930710e7230a4a6644cfa40e218e54
Uladzislau Sobal <sobolvladsav@gmail.com>
2023-03-24 13:25:19 +0300

    added .exemple.env

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function main(){
     let osuMParser = new OsuMatchParser({strictMPool: true}, matchLinks, mappool);
     osuMParser.parse()
     .then((res)=>{
-        //console.log(res[0].scores); 
+        console.log(res); 
         console.log(osuMParser.allScores);     
         console.log('Program endeded')
     });   
