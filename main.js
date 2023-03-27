3944c56bc43646f4171df244f0a19b1a77f2c7c0
Uladzislau Sobal <sobolvladsav@gmail.com>
2023-03-27 11:17:23 +0300

    fixed stringify and writting to file added

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const OsuMatchParser = require('./src/osuMatchParser');
 require('dotenv').config();
+const UserMatch = require('./src/userMatch');
+const fs = require('fs');
 
 function main(){
     const matchLinks = JSON.parse(process.env.MATCH_LINKS); 
@@ -12,12 +14,19 @@ function main(){
     .then((res)=>{
         console.log(res);       
         //getting json array of yser matches
-        console.log(JSON.stringify(res,function(key, value){
-            if (typeof value === 'UserMatch') {
+        let writeContent = JSON.stringify(res,function(key, value){
+           
+            if (typeof value === 'object' && value instanceof UserMatch)  {               
                 return value.getStaticJson();
               }
               return value;
-        }));
+        });
+        console.log(writeContent); 
+        fs.writeFile('qualsResults.json', writeContent, err => {
+            if (err) {
+              console.error(err);
+            }            
+          });
         console.log('Program endeded')
     });   
     
